482d4fc83500687e01254530fcd11f741fae26ea
Asuka Minato <i@asukaminato.eu.org>
2024-10-16 17:52:46 +0900

    now support java double[] and return String as example.

--- com/example/fsrs/FSRS.java
+++ com/example/fsrs/FSRS.java
@@ -8,25 +8,7 @@ public class FSRS {
     private static native long ParameterNew(FSRS callback,
             int maximum_interval,
             double request_retention,
-            double w1,
-            double w2,
-            double w3,
-            double w4,
-            double w5,
-            double w6,
-            double w7,
-            double w8,
-            double w9,
-            double w10,
-            double w11,
-            double w12,
-            double w13,
-            double w14,
-            double w15,
-            double w16,
-            double w17,
-            double w18,
-            double w19,
+            double[] w,
             double decay,
             double factor,
             boolean enable_short_term);
@@ -39,9 +21,15 @@ public class FSRS {
 
     private static native long RecordLogGet(FSRS callback, long card, long rating);
 
-    private static native long getCardScheduledDays(FSRS callback, long card);
+    private static native long CardScheduledDays(FSRS callback, long card);
+
+    private static native String CardScheduledtoString(FSRS callback, long card);
 
     private static native long SchedulingInfoCard(FSRS callback, long SchedulingInfo);
+
+    private static native long SchedulingInfoReviewLog(FSRS callback, long SchedulingInfo);
+
+    private static native String ReviewLogtoString(FSRS callback, long ReviewLog);
 
     static {
         System.loadLibrary("rs_fsrs_java");
@@ -50,19 +38,21 @@ public class FSRS {
     public static void main(String[] args) {
         FSRS gf = new FSRS();
         long fsrs_ = FsrsNew(gf, ParameterNew(gf, 90, 0.9,
-                0.5, 0.5, 0.5, 0.5, 0.5, //
-                0.5, 0.5, 0.5, 0.5, 0.5, //
-                0.5, 0.5, 0.5, 0.5, 0.5, //
-                0.5, 0.5, 0.5, 0.5,
+                new double[] {
+                        0.5, 0.5, 0.5, 0.5, 0.5,
+                        0.5, 0.5, 0.5, 0.5, 0.5,
+                        0.5, 0.5, 0.5, 0.5, 0.5,
+                        0.5, 0.5, 0.5, 0.5 },
                 0.9, 0.9, false));
         long fsrs = FsrsDefault(gf);
         long card = CardNew(gf);
         long scheduledCard = repeat(gf, fsrs, card, Instant.now().getEpochSecond());
         for (long rating : new long[] { 1, 2, 3, 4 }) {
             long scheduling_info = RecordLogGet(gf, scheduledCard, rating);
-            System.out.println(getCardScheduledDays(gf,
-                    SchedulingInfoCard(gf, scheduling_info)));
-
+            long card_ = SchedulingInfoCard(gf, scheduling_info);
+            long review_log = SchedulingInfoReviewLog(gf, scheduling_info);
+            System.out.println(CardScheduledtoString(gf, card_));
+            System.out.println(ReviewLogtoString(gf, review_log));
         }
     }
 }
