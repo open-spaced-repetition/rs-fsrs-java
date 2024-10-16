fb7c8aeb984f3acb7ddf9c73f44aef83e3cb3752
Asuka Minato <i@asukaminato.eu.org>
2024-10-16 11:52:10 +0900

    update

--- com/example/fsrs/FSRS.java
+++ com/example/fsrs/FSRS.java
@@ -3,13 +3,17 @@ package com.example.fsrs;
 import java.time.Instant;
 
 public class FSRS {
-    private static native long FsrsNew(FSRS callback);
+    private static native long FsrsDefault(FSRS callback);
 
     private static native long CardNew(FSRS callback);
 
-    private static native long schedule(FSRS callback, long fsrs, long card, long second);
-    private static native long selectCard(FSRS callback, long card, long rating);
-    private static native long getCardScheduledDays(long card);
+    private static native long repeat(FSRS callback, long fsrs, long card, long second);
+
+    private static native long RecordLogGet(FSRS callback, long card, long rating);
+
+    private static native long getCardScheduledDays(FSRS callback, long card);
+
+    private static native long SchedulingInfoCard(FSRS callback, long SchedulingInfo);
 
     static {
         System.loadLibrary("rs_fsrs_java");
@@ -17,10 +21,14 @@ public class FSRS {
 
     public static void main(String[] args) {
         FSRS gf = new FSRS();
-        long f = FsrsNew(gf);
-        long c = CardNew(gf);
-        long scheduledCard = schedule(gf, f, c, Instant.now().getEpochSecond());
-        long updated_card = selectCard(gf, scheduledCard, 4);
-        System.out.println(getCardScheduledDays(updated_card));
-    }
+        long fsrs = FsrsDefault(gf);
+        long card = CardNew(gf);
+        long scheduledCard = repeat(gf, fsrs, card, Instant.now().getEpochSecond());
+        for (long rating : new long[]{1,2,3,4}) {
+            long scheduling_info = RecordLogGet(gf, scheduledCard, rating);
+            System.out.println(getCardScheduledDays(gf,
+                     SchedulingInfoCard(gf, scheduling_info)));
+    
+        }
+     }
 }
