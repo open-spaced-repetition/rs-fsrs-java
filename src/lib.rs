4871e0dee8da09429dbd6d8044e4e5478ba1feed
Asuka Minato <i@asukaminato.eu.org>
2024-10-16 14:14:38 +0900

    at least it works

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use jni::JNIEnv;
 // references to Java objects remain valid)
 use jni::objects::{GlobalRef, JClass, JObject};
 
-use jni::sys::jlong;
+use jni::sys::{jboolean, jdouble, jfloat, jfloatArray, jint, jlong};
 
 fn to_raw<T>(value: T) -> jlong {
     Box::into_raw(Box::new(value)) as jlong
@@ -16,6 +16,11 @@ fn to_raw<T>(value: T) -> jlong {
 
 struct FSRS {
     inner: fsrs::FSRS,
+    callback: GlobalRef,
+}
+
+struct Parameter {
+    inner: fsrs::Parameters,
     callback: GlobalRef,
 }
 
@@ -28,6 +33,69 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_FsrsDefault(
     let global_ref = env.new_global_ref(callback).unwrap();
     to_raw(FSRS {
         inner: fsrs::FSRS::default(),
+        callback: global_ref,
+    })
+}
+
+#[no_mangle]
+/// TODO: pass array instead of 19 val
+pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ParameterNew(
+    env: JNIEnv,
+    _class: JClass,
+    callback: JObject,
+    maximum_interval: jint,
+    request_retention: jdouble,
+    w1: jdouble,
+    w2: jdouble,
+    w3: jdouble,
+    w4: jdouble,
+    w5: jdouble,
+    w6: jdouble,
+    w7: jdouble,
+    w8: jdouble,
+    w9: jdouble,
+    w10: jdouble,
+    w11: jdouble,
+    w12: jdouble,
+    w13: jdouble,
+    w14: jdouble,
+    w15: jdouble,
+    w16: jdouble,
+    w17: jdouble,
+    w18: jdouble,
+    w19: jdouble,
+    decay: jdouble,
+    factor: jdouble,
+    enable_short_term: jboolean,
+) -> jlong {
+    let global_ref = env.new_global_ref(callback).unwrap();
+    let w = [
+        w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16, w17, w18, w19,
+    ];
+    to_raw(Parameter {
+        inner: fsrs::Parameters {
+            maximum_interval,
+            request_retention,
+            w,
+            decay,
+            factor,
+            enable_short_term: enable_short_term == 0, // here is u8, 0 or 1
+        },
+        callback: global_ref,
+    })
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_FsrsNew(
+    env: JNIEnv,
+    _class: JClass,
+    callback: JObject,
+    parameter: jlong,
+) -> jlong {
+    let global_ref = env.new_global_ref(callback).unwrap();
+    let parameter = &*(parameter as *const Parameter);
+    to_raw(FSRS {
+        inner: fsrs::FSRS::new(parameter.inner),
         callback: global_ref,
     })
 }
