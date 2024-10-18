86f4ab86795f482399690ee0bde37203c5a79be4
Asuka Minato <i@asukaminato.eu.org>
2024-10-19 00:10:17 +0900

    seems global_ref is unnecessary

--- com/example/fsrs/FSRS.java
+++ com/example/fsrs/FSRS.java
@@ -3,9 +3,9 @@ package com.example.fsrs;
 import java.time.Instant;
 
 public class FSRS {
-    private static native long FsrsNew(FSRS callback, long parameter);
+    private static native long FsrsNew(long parameter);
 
-    private static native long ParameterNew(FSRS callback,
+    private static native long ParameterNew(
             int maximum_interval,
             double request_retention,
             double[] w,
@@ -13,46 +13,45 @@ public class FSRS {
             double factor,
             boolean enable_short_term);
 
-    private static native long FsrsDefault(FSRS callback);
+    private static native long FsrsDefault();
 
-    private static native long CardNew(FSRS callback);
+    private static native long CardNew();
 
-    private static native long repeat(FSRS callback, long fsrs, long card, long second);
+    private static native long repeat(long fsrs, long card, long second);
 
-    private static native long RecordLogGet(FSRS callback, long card, long rating);
+    private static native long RecordLogGet(long card, long rating);
 
-    private static native long CardScheduledDays(FSRS callback, long card);
+    private static native long CardScheduledDays(long card);
 
-    private static native String CardScheduledtoString(FSRS callback, long card);
+    private static native String CardScheduledtoString(long card);
 
-    private static native long SchedulingInfoCard(FSRS callback, long SchedulingInfo);
+    private static native long SchedulingInfoCard(long SchedulingInfo);
 
-    private static native long SchedulingInfoReviewLog(FSRS callback, long SchedulingInfo);
+    private static native long SchedulingInfoReviewLog(long SchedulingInfo);
 
-    private static native String ReviewLogtoString(FSRS callback, long ReviewLog);
+    private static native String ReviewLogtoString(long ReviewLog);
 
     static {
         System.loadLibrary("rs_fsrs_java");
     }
 
     public static void main(String[] args) {
-        FSRS gf = new FSRS();
-        long fsrs_ = FsrsNew(gf, ParameterNew(gf, 90, 0.9,
+        long fsrs_ = FsrsNew(ParameterNew(90, 0.9,
                 new double[] {
                         0.5, 0.5, 0.5, 0.5, 0.5,
                         0.5, 0.5, 0.5, 0.5, 0.5,
                         0.5, 0.5, 0.5, 0.5, 0.5,
                         0.5, 0.5, 0.5, 0.5 },
                 0.9, 0.9, false));
-        long fsrs = FsrsDefault(gf);
-        long card = CardNew(gf);
-        long scheduledCard = repeat(gf, fsrs, card, Instant.now().getEpochSecond());
+        long fsrs = FsrsDefault();
+        long card = CardNew();
+        long scheduledCard = repeat(fsrs, card, Instant.now().getEpochSecond());
         for (long rating : new long[] { 1, 2, 3, 4 }) {
-            long scheduling_info = RecordLogGet(gf, scheduledCard, rating);
-            long card_ = SchedulingInfoCard(gf, scheduling_info);
-            long review_log = SchedulingInfoReviewLog(gf, scheduling_info);
-            System.out.println(CardScheduledtoString(gf, card_));
-            System.out.println(ReviewLogtoString(gf, review_log));
+            long scheduling_info = RecordLogGet(scheduledCard, rating);
+            long card_ = SchedulingInfoCard(scheduling_info);
+            long review_log = SchedulingInfoReviewLog(scheduling_info);
+            System.out.println(CardScheduledtoString(card_));
+            System.out.println(ReviewLogtoString(review_log));
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use jni::JNIEnv;
 // They carry extra lifetime information to prevent them escaping from the
 // current local frame (which is the scope within which local (temporary)
 // references to Java objects remain valid)
-use jni::objects::{GlobalRef, JClass, JDoubleArray, JObject, JString};
+use jni::objects::{JClass, JDoubleArray, JString};
 
 use jni::sys::{jboolean, jdouble, jint, jlong};
 
@@ -19,29 +19,23 @@ fn to_raw<T>(value: T) -> jlong {
 
 struct FSRS {
     inner: fsrs::FSRS,
-    callback: GlobalRef,
 }
 
 struct Parameter {
     inner: fsrs::Parameters,
-    callback: GlobalRef,
 }
 
 struct ReviewLog {
     inner: fsrs::ReviewLog,
-    callback: GlobalRef,
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_FsrsDefault(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
 ) -> jlong {
-    let global_ref = env.new_global_ref(callback).unwrap();
     to_raw(FSRS {
         inner: fsrs::FSRS::default(),
-        callback: global_ref,
     })
 }
 
@@ -49,7 +43,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_FsrsDefault(
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ParameterNew(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     maximum_interval: jint,
     request_retention: jdouble,
     w: JDoubleArray,
@@ -57,7 +50,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ParameterNew(
     factor: jdouble,
     enable_short_term: jboolean,
 ) -> jlong {
-    let global_ref = env.new_global_ref(callback).unwrap();
     let mut buf = vec![0f64; 19];
     env.get_double_array_region(w, 0, &mut buf)
         .expect("too much information");
@@ -70,7 +62,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ParameterNew(
             factor,
             enable_short_term: enable_short_term == (true as u8), // here is u8, 0 or 1
         },
-        callback: global_ref,
     })
 }
 
@@ -78,77 +69,63 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ParameterNew(
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_FsrsNew(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     parameter: jlong,
 ) -> jlong {
-    let global_ref = env.new_global_ref(callback).unwrap();
     let parameter = unsafe { &*(parameter as *const Parameter) };
     to_raw(FSRS {
         inner: fsrs::FSRS::new(parameter.inner),
-        callback: global_ref,
     })
 }
 
 struct Card {
     inner: fsrs::Card,
-    callback: GlobalRef,
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_CardNew(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
 ) -> jlong {
-    let global_ref = env.new_global_ref(callback).unwrap();
     to_raw(Card {
         inner: fsrs::Card::new(),
-        callback: global_ref,
     })
 }
 
 struct RecordLog {
     inner: fsrs::RecordLog,
-    callback: GlobalRef,
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_repeat(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     fsrs_: jlong,
     card: jlong,
     n: jlong,
 ) -> jlong {
     let f = unsafe { &*(fsrs_ as *const FSRS) };
     let card = unsafe { &*(card as *const Card) };
-    let global_ref = env.new_global_ref(callback).unwrap();
 
     to_raw(RecordLog {
         inner: f.inner.repeat(
             card.inner.clone(),
             chrono::DateTime::from_timestamp(n as i64, 0).expect("time error"),
         ),
-        callback: global_ref,
     })
 }
 
 struct SchedulingInfo {
     inner: fsrs::SchedulingInfo,
-    callback: GlobalRef,
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_RecordLogGet(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     scheduling_info: jlong,
     rating: jlong,
 ) -> jlong {
     let f = unsafe { &*(scheduling_info as *const RecordLog) };
-    let global_ref = env.new_global_ref(callback).unwrap();
 
     to_raw(SchedulingInfo {
         inner: f
@@ -162,7 +139,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_RecordLogGet(
             })
             .unwrap()
             .to_owned(),
-        callback: global_ref,
     })
 }
 
@@ -170,14 +146,11 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_RecordLogGet(
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_SchedulingInfoCard(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     scheduling_info: jlong,
 ) -> jlong {
     let f = unsafe { &*(scheduling_info as *const SchedulingInfo) };
-    let global_ref = env.new_global_ref(callback).unwrap();
     to_raw(Card {
         inner: f.inner.card.clone(),
-        callback: global_ref,
     })
 }
 
@@ -185,14 +158,11 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_SchedulingInfoCard(
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_SchedulingInfoReviewLog(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     scheduling_info: jlong,
 ) -> jlong {
     let f = unsafe { &*(scheduling_info as *const SchedulingInfo) };
-    let global_ref = env.new_global_ref(callback).unwrap();
     to_raw(ReviewLog {
         inner: f.inner.review_log.clone(),
-        callback: global_ref,
     })
 }
 
@@ -200,7 +170,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_SchedulingInfoReviewLog
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_CardScheduledDays(
     env: JNIEnv,
     _class: JClass,
-    callback: JObject,
     card: jlong,
 ) -> jlong {
     let c = unsafe { &*(card as *const Card) };
@@ -210,7 +179,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_CardScheduledDays(
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_CardScheduledtoString<'a>(
     env: JNIEnv<'a>,
     _class: JClass<'a>,
-    callback: JObject<'a>,
     card: jlong,
 ) -> JString<'a> {
     let c = unsafe { &*(card as *const Card) };
@@ -222,7 +190,6 @@ pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_CardScheduledtoString<'
 pub unsafe extern "system" fn Java_com_example_fsrs_FSRS_ReviewLogtoString<'a>(
     env: JNIEnv<'a>,
     _class: JClass<'a>,
-    callback: JObject<'a>,
     card: jlong,
 ) -> JString<'a> {
     let c = unsafe { &*(card as *const ReviewLog) };
